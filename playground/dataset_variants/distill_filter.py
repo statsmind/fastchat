c4d8391ceda5e8b39318dce3db43f7599fcfcffb
Ying Sheng <sqy1415@gmail.com>
2023-09-01 19:44:09 -0700

    update

--- playground/dataset_variants/distill_filter.py
+++ playground/dataset_variants/distill_filter.py
@@ -2,9 +2,25 @@ import argparse
 import json
 
 
+def get_distill_data(filepath):
+    with open(filepath, "r") as f:
+        all_data = json.load(f)
+    print(all_data[0].keys())
+
+    distill_data = []
+    for conv in all_data:
+        if conv["model"] in ["claude-1", "claude-instant-1", "gpt-4", "claude-2"]:
+            distill_data.apend(conv)
+
+    models = set()
+    for conv in distill_data:
+        models.add(conv["model"])
+    print(models)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--filepath", type=str, default="~/arena_dataset/clean_conv_20230809_1M.json")
+    parser.add_argument("--filepath", type=str, default="clean_conv_20230809_1M.json")
     args = parser.parse_args()
 
     distill_data = get_distill_data(args.filepath)

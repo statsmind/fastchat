927cf5254749d43b563ad3fec13eada460a457eb
Ying Sheng <sqy1415@gmail.com>
2023-09-02 12:34:13 -0700

    update

--- playground/dataset_variants/format_trans.py
+++ playground/dataset_variants/format_trans.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
         conversation = []
         for chat in conv["conversation"]:
             conversation.append({"from": chat["role"], "value": chat["content"]})
-        train_data.append({"id": conv["conversation_id"], "conversation": conversation})
+        train_data.append({"id": conv["conversation_id"], "conversations": conversation})
 
     with open(args.output_file, "w") as f:
         json.dump(train_data, f)

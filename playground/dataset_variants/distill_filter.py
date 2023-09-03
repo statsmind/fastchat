3fae52e3e1710db8999ece08d2094eed8262c3aa
Ying Sheng <sqy1415@gmail.com>
2023-09-03 09:56:39 -0700

    update

--- playground/dataset_variants/distill_filter.py
+++ playground/dataset_variants/distill_filter.py
@@ -22,25 +22,9 @@ def gen_distill_data(filepath, output_file):
         json.dump(distill_data, f, ensure_ascii=False)
 
 
-def gen_upvote_data(filepath, output_file):
-    with open(filepath, "r") as f:
-        all_data = json.load(f)
-    print(all_data[0])
-    print(all_data[0]["conversation"].keys())
-
-    # upvote_data = []
-    # for conv in all_data:
-    #     if conv["model"] in ["claude-1", "claude-instant-1", "gpt-4", "claude-2"]:
-    #         distill_data.append(conv)
-    # print("distill data size", len(distill_data))
-
-    # with open(output_file, "w") as f:
-    #     json.dump(distill_data, f)
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--filepath", type=str, default="clean_conv_20230809_1M.json")
+    parser.add_argument("--input-file", type=str, default="clean_conv_20230809_1M.json")
     parser.add_argument("--output-file", type=str, default="distill.json")
     args = parser.parse_args()
 
---
+++ playground/dataset_variants/upvote_filter.py
@@ -0,0 +1,27 @@
+import argparse
+import json
+
+
+def gen_upvote_data(input_file, output_file):
+    with open(input_file, "r") as f:
+        all_data = json.load(f)
+    print(all_data[0])
+    print(all_data[0].keys())
+
+    upvote_data = []
+    for conv in all_data:
+        if conv["model"] not in ["claude-1", "claude-instant-1", "gpt-4", "claude-2", "palm-2", "gpt-3.5-turbo"]:
+            upvote_data.append(conv)
+    print("upvote data size", len(upvote_data))
+
+    with open(output_file, "w") as f:
+        json.dump(upvote_data, f)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input-file", type=str, default="clean_mergedvote_conv_20230831.json")
+    parser.add_argument("--output-file", type=str, default="upvote.json")
+    args = parser.parse_args()
+
+    upvote_data = gen_upvote_data(args.input_file, args.output_file)

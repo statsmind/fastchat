9f251b64f69f9fa0ce92513a90c6adc228bfd66c
Lianmin Zheng <lianminzheng@gmail.com>
2023-09-19 11:47:17 +0000

    update for non-legacy tokenizer

--- fastchat/train/train.py
+++ fastchat/train/train.py
@@ -133,6 +133,9 @@ def preprocess(
             # "-2" is hardcoded for the LLaMA tokenizer to make the offset correct.
             instruction_len = len(tokenizer(parts[0]).input_ids) - 2
 
+            if i != 0 and not tokenizer.legacy:
+                instruction_len -= 1
+
             # Ignore the user instructions
             target[cur_len : cur_len + instruction_len] = IGNORE_TOKEN_ID
             cur_len += turn_len
